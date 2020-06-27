442257f333bab259ac40e8cf7b0f2c359e34d5f7
Nick <darkpwnss@hotmail.com>
2020-06-28 00:12:49 +0200

    heroku db update

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql');
 
 module.exports = connection = mysql.createConnection({
-  host     : 'database host',
-  user     : 'database user',
-  password : 'database password',
-  database : 'database name'
+  host: 'fdb22.awardspace.net',
+  user: '3490672_scraper',
+  password: 'escape123',
+  database: '3490672_scraper',
+  port: 3306
 });
