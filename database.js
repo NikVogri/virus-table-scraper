a07157154fa452fb0d16393d81139519037a8944
Nick <darkpwnss@hotmail.com>
2020-07-07 00:12:15 +0200

    removed logs & redundant code

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 module.exports = connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: process.env.DB_NAME,
-  port: 3306
+  host: "host",
+  user: "user",
+  password: "password",
+  database: "database",
+  port: 3306,
 });
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const colors = require("colors");
 const scraper = require("table-scraper");
 const app = require("express")();
 const cors = require("cors");
-const myql = require('mysql');
-const dbConnection = require('./database');
+const dbConnection = require("./database");
 
 app.use(cors());
 
@@ -23,40 +22,45 @@ app.listen(process.env.PORT || 3000, () => {
 // Website
 const url = "https://www.worldometers.info/coronavirus/";
 
-
 const updateDatabase = () => {
   scraper
     .get(url)
     .then(async (res) => {
       console.log("Starting scrape".yellow.inverse);
       const data = cleanData(res);
-      //console.log(data);
-      const countryData = data.countryData.map(country => getData(country));
-      const continentData = data.continentData.map(continent => getData(continent));
-      // Get array inside array from object for mysql insertion
-      const countryDataArray = countryData.map(countryObj => {
-        return Object.values(countryObj);
-      })
 
-      const continentDataArray = continentData.map(continentObj => {
+      const countryData = data.countryData.map((country) => getData(country));
+      const continentData = data.continentData.map((continent) =>
+        getData(continent)
+      );
+      // Get array inside array from object for mysql insertion
+      const countryDataArray = countryData.map((countryObj) => {
+        return Object.values(countryObj);
+      });
+
+      const continentDataArray = continentData.map((continentObj) => {
         return Object.values(continentObj);
-      })
+      });
 
       // Update database
       dbConnection.connect();
 
       // delete data from the same day
-      dbConnection.query('DELETE FROM countries WHERE DATE(created_at) = CURDATE() OR DATE(created_at) = CURDATE() - INTERVAL 1 WEEK',
-      (err, res) => {
-        if (err) throw err;
-        console.log("Deleted country data for current day".yellow.inverse);
-      });
+      dbConnection.query(
+        "DELETE FROM countries WHERE DATE(created_at) = CURDATE() OR DATE(created_at) = CURDATE() - INTERVAL 1 WEEK",
+        (err, res) => {
+          if (err) throw err;
+          console.log("Deleted country data for current day".yellow.inverse);
+        }
+      );
 
-      dbConnection.query('DELETE FROM continents WHERE DATE(created_at) = CURDATE() OR DATE(created_at) = CURDATE() - INTERVAL 1 WEEK',
-      (err, res) => {
-        if (err) throw err;
-        console.log("Deleted continent data for current day".yellow.inverse);
-      });
+      dbConnection.query(
+        "DELETE FROM continents WHERE DATE(created_at) = CURDATE() OR DATE(created_at) = CURDATE() - INTERVAL 1 WEEK",
+        (err, res) => {
+          if (err) throw err;
+          console.log("Deleted continent data for current day".yellow.inverse);
+        }
+      );
 
       const continentQuery = `INSERT INTO continents (
       continent,
@@ -81,14 +85,14 @@ const updateDatabase = () => {
       dbConnection.query(continentQuery, [continentDataArray], (err, res) => {
         if (err) throw err;
         console.log("Write to continents complete".green.inverse);
-      })
+      });
 
       dbConnection.query(countryQuery, [countryDataArray], (err, res) => {
         if (err) throw err;
         console.log("Write to countries complete".green.inverse);
-      })
+      });
 
-     dbConnection.end();
+      dbConnection.end();
     })
     .catch((err) => console.log(err.message.red.inverse));
 };
@@ -112,10 +116,11 @@ const cleanData = (res) => {
       deleteIndex.push(index);
     }
   });
+
   // remove objects that do not contain useful data
-  deleteIndex.forEach((i) =>
-    filthyData.slice(0, i).concat(filthyData.slice(-i))
-  );
+  deleteIndex.forEach((i) => {
+    filthyData.slice(0, i).concat(filthyData.slice(-i));
+  });
 
   // clean data
   const cleanData = filthyData.filter(
@@ -124,41 +129,42 @@ const cleanData = (res) => {
 
   // remove continent data from country array
   const continents = [
-  "Europe",
-  "Africa",
-  "North America",
-  "Asia",
-  "South America",
-  "Oceania",
-  "World",
-];
+    "Europe",
+    "Africa",
+    "North America",
+    "Asia",
+    "South America",
+    "Oceania",
+    "World",
+  ];
+
   const countryData = cleanData.filter(
-  (el) => !continents.includes(el.country)
-);
+    (el) => !continents.includes(el.country)
+  );
 
-const continentData = cleanData.filter(
-(el) => continents.includes(el.country)
-);
+  const continentData = cleanData.filter((el) =>
+    continents.includes(el.country)
+  );
 
-  return {countryData, continentData};
+  return { countryData, continentData };
 };
 
 const cleanInt = (num) => {
-  if (num && num !== 'N/A') {
-  const cleanStringNum = num.replace('+', '');
-  return parseInt(cleanStringNum.split(',').join(''));
-} else return 0;
-}
+  if (num && num !== "N/A") {
+    const cleanStringNum = num.replace("+", "");
+    return parseInt(cleanStringNum.split(",").join(""));
+  } else return 0;
+};
 
-const getData = data => {
+const getData = (data) => {
   return {
-    continent: data.Continent || '/',
+    continent: data.Continent || "/",
     country: data.country,
     totalCases: cleanInt(data.TotalCases),
     newCases: cleanInt(data.NewCases),
     totalDeaths: cleanInt(data.TotalDeaths),
     newDeaths: cleanInt(data.NewDeaths),
     activeCases: cleanInt(data.ActiveCases),
-    casePerPop: cleanInt(data['1 Caseevery X ppl'])
-  }
-}
+    casePerPop: cleanInt(data["1 Caseevery X ppl"]),
+  };
+};
